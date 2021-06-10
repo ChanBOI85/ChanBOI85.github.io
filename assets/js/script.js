57f824e274c86a77b707492f27f19c54b9f5a6e4
ChanBOI85 <63307738+ChanBOI85@users.noreply.github.com>
2021-06-10 21:53:06 +0800

    Update

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// Cart Page
+$(document).ready(function() {
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+});
+
+function menuOn() {
+    document.getElementById("menu").style.display = "block";
+  }
+  
+  function menuOff() {
+    document.getElementById("menu").style.display = "none";
+  }
