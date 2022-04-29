a987a1b8526561aae874a1a3553df75a0b4248b8
ChanBOI85 <63307738+ChanBOI85@users.noreply.github.com>
2022-04-29 17:41:16 +0800

    Update sticky-code.js

--- js/sticky-code.js
+++ js/sticky-code.js
@@ -1,14 +1,34 @@
-function copyCode() {
-    var code = document.getElementById("code");
-
-    // copy the code
-    navigator.clipboard
-        .writeText(code.innerText)
-        .then(() => {
-            code.style.color = "lightgreen"
+function copyCode(text) {
+    
+    if (navigator.clipboard) {
+        // copy the code
+        navigator.clipboard
+            .writeText(text)
+            .then(() => {
+                code.style.color = "lightgreen"
+            })
+            .catch(() => {
+                code.style.color = "red"
+            });
+    } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+        document.getElementById("note").innerText = "Click anywhere to copy the code";
+        
+        document.querySelector('body').addEventListener('click', function() {
+            var textarea = document.createElement("textarea");
+            textarea.textContent = text;
+            textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in Microsoft Edge.
+            document.body.appendChild(textarea);
+            textarea.select();
+            try {
+                return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+            }
+            catch (ex) {
+                console.warn("Copy to clipboard failed.", ex);
+                return prompt("Copy to clipboard: Ctrl+C, Enter", text);
+            }
+            finally {
+                document.body.removeChild(textarea);
+            }
         })
-        .catch(() => {
-            code.style.color = "red"
-        });
-
+    }
 }
