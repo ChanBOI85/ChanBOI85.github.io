aedad2dfca4854638f51b4d60929cf2f6c920c49
ChanBOI85 <63307738+ChanBOI85@users.noreply.github.com>
2022-05-04 00:16:37 +0800

    Add Scroll Effect Brands page

--- js/script.js
+++ js/script.js
@@ -31,43 +31,43 @@ function openTab(evt, tabName) {
   evt.currentTarget.className += " active";
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "flex";  
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}    
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";  
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "flex";  
   
-  if (slideIndex === 5) {
-    document.getElementById("next").style.display = "none"
-  } else {
-    document.getElementById("next").style.display = "block";
-    document.getElementById("prev").style.display = "block";
-  }
+//   if (slideIndex === 5) {
+//     document.getElementById("next").style.display = "none"
+//   } else {
+//     document.getElementById("next").style.display = "block";
+//     document.getElementById("prev").style.display = "block";
+//   }
 
-  if (slideIndex === 1) {
-    document.getElementById("prev").style.display = "none";
-  }
+//   if (slideIndex === 1) {
+//     document.getElementById("prev").style.display = "none";
+//   }
   
-}
+// }
 
 var cw = $('#contactCircle').width();
 $('#contactCircle').css({'height':cw+'px'});
